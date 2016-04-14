f00a64b3acc2f326c6b5e716f6babcf8fdfaa0b1
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-14 11:40:48 +0200

    Detect another instance of this extension and kill self if there's another
    This is especially useful when both release & dev extensions are enabled

--- js/content-script.js
+++ js/content-script.js
@@ -1,5 +1,21 @@
 // TODO tighten test
 if (document.title.indexOf('Freebox OS') == 0) {
+	var meta_name = 'Material-Freebox-OS';
+
+	// Check if Material-Freebox-OS meta tag is here
+	// (it means that another instance has already been injected)
+	var metas = document.getElementsByTagName('meta');
+	for (var i=0; i<metas.length; i++) {
+		if (metas[i].name == meta_name)
+			throw new Error('Material-Freebox-OS already injected, aborting.');
+	}
+
+	// Append Material-Freebox-OS meta tag to head
+	var m = document.createElement('meta');
+	m.name = meta_name;
+	m.content = 'true';
+	document.head.appendChild(m);
+
 	// Inject script
 	injectScript(chrome.extension.getURL('js/script.js'));
 
