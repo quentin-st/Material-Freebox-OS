44de8930f2e694ce27355041459721cda6470326
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-16 22:04:40 +0200

    Release script: introduce flavours

--- prepare-release.py
+++ prepare-release.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import argparse
 import sys
 import os
 import shutil
 import json
 
+
+def get_version(flavour):
+    manifest = manifests[flavour]
+
+    # Parse file depending on flavour
+    if flavour == 'chrome':
+        manifest_json = open(manifest)
+        return json.load(manifest_json)['version']
+    else:
+        return None
+
 output_dir = 'release/'
-manifest = 'manifest.json'
-files = [
+flavours = ['chrome']
+files_generic = [
     'manifest.json',
     '3d/*',
     'css/style.css',
     'img/*',
     'js/*'
 ]
+files_flavours = {
+    'chrome': [
+        'manifest.json'
+    ]
+}
+manifests = {
+    'chrome': 'manifest.json'
+}
+
+parser = argparse.ArgumentParser(description='Prepare release packages for different flavours')
+parser.add_argument('--flavour')
+args = parser.parse_args()
+flavour = args.flavour
+
+if flavour is None:
+    print('Please specify a flavour using --flavour')
+    sys.exit(1)
+elif flavour not in flavours:
+    print('Unknown flavour, exiting')
+    sys.exit(1)
 
 # Create output dir if necessary
 if not os.path.isdir(output_dir):
@@ -22,15 +54,12 @@ if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 
 # Read manifest & read version name
-if not os.path.isfile(manifest):
-    print('Missing manifest file ({})'.format(manifest))
-
-manifest_json = open(manifest)
-version = json.load(manifest_json)['version']
-output_dir = os.path.join(output_dir, 'Material-Freebox-OS-{}'.format(version))
+version = get_version(flavour)
+output_dir = os.path.join(output_dir, 'Material-Freebox-OS-{}-{}'.format(flavour, version))
 
 # Expand files list (js/* => [js/script.js, js/injecter.js]
 expanded_files = []
+files = files_generic + files_flavours[flavour]
 for file in files:
     if file.endswith('*'):
         real_name = file[:-1]
