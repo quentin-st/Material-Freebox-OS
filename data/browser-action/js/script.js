f9b2c704e02f40b079187ae7ca47cff4ec563e60
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-23 15:36:19 +0200

    Allow setting a custom wallpaper from its URL

--- data/browser-action/js/script.js
+++ data/browser-action/js/script.js
@@ -4,23 +4,28 @@
  */
 (function() {
     var BrowserAction = {
+        updateWallpaperAndSave: function(uri) {
+            BrowserAction.updateWallpaperInTab(uri, MaterialFreeboxOS.findWallpaperInfos(uri));
+            chrome.storage.sync.set({
+                'wallpaper': uri
+            });
+        },
+
         /**
          * Updates wallpaper in current tab
          * @param projectUri
          * @param wallpaperInfos
          */
         updateWallpaperInTab: function(projectUri, wallpaperInfos) {
-            var uri = chrome.extension.getURL(projectUri);
+            var uri = MaterialFreeboxOS.getDepURI(projectUri);
 
             chrome.tabs.getSelected(null, function(tab) {
                 if (MaterialFreeboxOS.matches(tab.title)) {
                     var updateWallpaper = function(uri, wallpaperInfos) {
                         document.body.style.backgroundImage = "url(" + uri + ")";
 
-                        if (wallpaperInfos != undefined) {
-                            var element = document.getElementsByClassName('desktop-wallpaper-credits')[0];
-                            MaterialFreeboxOS.updateWallpaperCredits(element, wallpaperInfos);
-                        }
+                        var element = document.getElementsByClassName('desktop-wallpaper-credits')[0];
+                        MaterialFreeboxOS.updateWallpaperCredits(element, wallpaperInfos);
                     };
 
                     var code =
@@ -37,7 +42,7 @@
 
     $(document).ready(function () {
         // Wallpapers
-        var wallpapersUl = $('.wallpapers');
+        var wallpapersUl = $('#wallpapers-images');
 
         // Inflate list
         MaterialFreeboxOS.wallpapers.forEach(function(wallpaper) {
@@ -46,25 +51,44 @@
                 .attr('data-credits', wallpaper.credits)
                 .attr('data-source', wallpaper.source)
                 .css('background-image', "url(" + MaterialFreeboxOS.getDepURI(wallpaper.image) + ")")
-                .appendTo(wallpapersUl);
+                .prependTo(wallpapersUl);
         });
-        var wallpapers = wallpapersUl.find('li');
+        var wallpapers_images = wallpapersUl.find('li'),
+            wallpapers_url = $('#wallpapers-url');
 
         // Retrieve current settings
         chrome.storage.sync.get('wallpaper', function(data) {
-            var defaultWallpaper = wallpapers.first().data('uri'),
-                settingsWallpaper = data['wallpaper'] || defaultWallpaper;
+            var defaultWallpaper = wallpapers_images.first().data('uri'),
+                wallpaper = data['wallpaper'] || defaultWallpaper;
 
-            wallpapers.filter('[data-uri="' + settingsWallpaper + '"]').addClass('current');
+            if (MaterialFreeboxOS.findWallpaperInfos(wallpaper) != null)
+                wallpapers_images.filter('[data-uri="' + wallpaper + '"]').addClass('current');
+            else
+                wallpapers_url.val(wallpaper);
         });
 
-        // Update current wallpaper
-        wallpapers.click(function () {
-            var uri = $(this).data('uri');
-            wallpapers.removeClass('current');
+        // Update current wallpaper: image
+        wallpapers_images.click(function () {
+            wallpapers_images.removeClass('current');
             $(this).addClass('current');
+            wallpapers_url.val('');
 
-            BrowserAction.updateWallpaperInTab(uri, MaterialFreeboxOS.findWallpaperInfos(uri));
+            BrowserAction.updateWallpaperAndSave($(this).data('uri'));
+        });
+
+        // Update current wallpaper: input
+        wallpapers_url.keyup(function(e) {
+            if (e.which == 13) {
+                wallpapers_images.removeClass('current');
+
+                BrowserAction.updateWallpaperAndSave($(this).val());
+            }
+        });
+
+        $('input[type="submit"]').click(function() {
+            var uri = $('input[type="file"]').val();
+
+            BrowserAction.updateWallpaperInTab(uri, null);
             chrome.storage.sync.set({
                 'wallpaper': uri
             });
