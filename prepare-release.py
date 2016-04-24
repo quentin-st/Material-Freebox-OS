f4d85e24a31124bbe65f5880df0c37fa6a1798c6
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-24 15:08:45 +0200

    prepare-release.py: Add ability to release all flavours from one single command

--- prepare-release.py
+++ prepare-release.py
@@ -33,6 +33,65 @@ files_flavours = {
 }
 manifest = 'manifest.json'
 
+
+def do_release(flavour):
+    # Open manifest & read version name
+    with open(manifest) as manifest_file:
+        manifest_json = json.load(manifest_file)
+    version = manifest_json['version']
+    output_dir_name = 'Material-Freebox-OS-{}-{}'.format(flavour, version)
+    output_dir = os.path.join(release_dir, output_dir_name)
+
+    # Expand files list (js/* => [js/script.js, js/injector.js]
+    expanded_files = []
+    files = files_generic + files_flavours[flavour]
+    for file in files:
+        if file.endswith('*'):
+            real_name = file[:-1]
+            expanded_files.extend(
+                [os.path.join(dp, f) for dp, dn, filenames in os.walk(real_name) for f in filenames]
+            )
+        else:
+            expanded_files.append(file)
+
+    # Copy these!
+    print('Copying resources in {}...'.format(output_dir))
+    for file in expanded_files:
+        destination = os.path.join(output_dir, file)
+        destination_dirs = os.path.dirname(destination)
+
+        if not os.path.isdir(destination_dirs):
+            os.makedirs(destination_dirs)
+
+        shutil.copy(file, destination)
+        print('\tCopied {}'.format(file, destination))
+
+    # With Chrome flavour: rewrite manifest to remove Firefox's specific nodes
+    if flavour == 'Chrome':
+        with open(os.path.join(output_dir, manifest), 'w') as output_manifest_file:
+            del manifest_json['applications']
+            json.dump(manifest_json, output_manifest_file)
+
+    # Create final ZIP package
+    zip_extension = 'xpi' if flavour == 'Firefox' else 'zip'
+    zip_name = 'Material-Freebox-OS-{}-{}.{}'.format(flavour, version, zip_extension)
+
+    with zipfile.ZipFile(os.path.join(release_dir, zip_name), 'w') as zip:
+        print('Creating package {}'.format(zip_name))
+
+        for root, dirs, files in os.walk(output_dir):
+            for file in files:
+                zip_filepath = os.path.relpath(os.path.join(root, file), output_dir)
+                zip.write(os.path.join(root, file), zip_filepath)
+
+                print('\tAdded {}'.format(zip_filepath))
+
+    shutil.rmtree(output_dir)
+    print('Deleted working directory {}'.format(output_dir))
+
+    print('Release file {} created for flavour {}'.format(zip_name, flavour))
+
+
 parser = argparse.ArgumentParser(description='Prepare release packages for different flavours')
 parser.add_argument('--flavour')
 args = parser.parse_args()
@@ -41,7 +100,7 @@ flavour = args.flavour
 if flavour is None:
     print('Please specify a flavour using --flavour')
     sys.exit(1)
-elif flavour not in flavours:
+elif flavour != 'all' and flavour not in flavours:
     print('Unknown flavour, exiting')
     sys.exit(1)
 
@@ -50,58 +109,8 @@ if not os.path.isdir(release_dir):
     print('Creating output dir')
     os.makedirs(release_dir)
 
-# Open manifest & read version name
-with open(manifest) as manifest_file:
-    manifest_json = json.load(manifest_file)
-version = manifest_json['version']
-output_dir_name = 'Material-Freebox-OS-{}-{}'.format(flavour, version)
-output_dir = os.path.join(release_dir, output_dir_name)
-
-# Expand files list (js/* => [js/script.js, js/injector.js]
-expanded_files = []
-files = files_generic + files_flavours[flavour]
-for file in files:
-    if file.endswith('*'):
-        real_name = file[:-1]
-        expanded_files.extend(
-            [os.path.join(dp, f) for dp, dn, filenames in os.walk(real_name) for f in filenames]
-        )
-    else:
-        expanded_files.append(file)
-
-# Copy these!
-print('Copying resources in {}...'.format(output_dir))
-for file in expanded_files:
-    destination = os.path.join(output_dir, file)
-    destination_dirs = os.path.dirname(destination)
-
-    if not os.path.isdir(destination_dirs):
-        os.makedirs(destination_dirs)
-
-    shutil.copy(file, destination)
-    print('\tCopied {}'.format(file, destination))
-
-# With Chrome flavour: rewrite manifest to remove Firefox's specific nodes
-if flavour == 'Chrome':
-    with open(os.path.join(output_dir, manifest), 'w') as output_manifest_file:
-        del manifest_json['applications']
-        json.dump(manifest_json, output_manifest_file)
-
-# Create final ZIP package
-zip_extension = 'xpi' if flavour == 'Firefox' else 'zip'
-zip_name = 'Material-Freebox-OS-{}-{}.{}'.format(flavour, version, zip_extension)
-
-with zipfile.ZipFile(os.path.join(release_dir, zip_name), 'w') as zip:
-    print('Creating package {}'.format(zip_name))
-
-    for root, dirs, files in os.walk(output_dir):
-        for file in files:
-            zip_filepath = os.path.relpath(os.path.join(root, file), output_dir)
-            zip.write(os.path.join(root, file), zip_filepath)
-
-            print('\tAdded {}'.format(zip_filepath))
-
-shutil.rmtree(output_dir)
-print('Deleted working directory {}'.format(output_dir))
-
-print('Release file {} created for flavour {}'.format(zip_name, flavour))
+if flavour == 'all':
+    for flavour in flavours:
+        do_release(flavour)
+else:
+    do_release(flavour)
