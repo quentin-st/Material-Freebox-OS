48655ac2013736f836d2683cc0cbedcf3c50fc86
Quentin Stoeckel <chteuchteu@gmail.com>
2016-04-28 21:34:53 +0200

    Add the whole material palette to available colors, use 500, 600, 700, 800 & 900 colors instead of darkening the 500 version

--- data/js/material-freebox-os.js
+++ data/js/material-freebox-os.js
@@ -23,18 +23,25 @@
 
         primaryColor: {
             colors: [
-                '#2196F3', // blue
-                '#F55145', // red
+                '#F44336', // red
+                '#E91E63', // pink
+                '#9C27B0', // purple
                 '#673AB7', // deep-purple
                 '#3F51B5', // indigo
+                '#2196F3', // blue
+                '#03A9F4', // light-blue
+                '#00BCD4', // cyan
                 '#009688', // teal
                 '#4CAF50', // green
                 '#8BC34A', // light-green
+                '#CDDC39', // lime
+                '#FFEB3B', // yellow
                 '#FFC107', // amber
                 '#FF9800', // orange
                 '#FF5722', // deep-orange
                 '#795548', // brown
-                '#607D8B'  // blue-grey
+                '#9E9E9E', // grey
+                '#607D8B', // blue-grey
             ],
 
             get defaultColor() {
